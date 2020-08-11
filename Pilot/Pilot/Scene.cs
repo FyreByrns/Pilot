3594411422e38ef9b48c717f2e9e05b61cfd4cfa
aaay-aaay <aaayaaay58@gmail.com>
2020-08-11 19:43:20 +0100

    Move level editor to editor button instead of settings

--- Pilot/Pilot/Scene.cs
+++ Pilot/Pilot/Scene.cs
@@ -1,4 +1,6 @@
-﻿namespace Pilot {
+﻿using PixelEngine;
+
+namespace Pilot {
     /// <summary>
     /// A state of the game.
     /// </summary>
@@ -9,6 +11,10 @@
 
         protected Scene(Game game) {
             this.game = game;
+        }
+        
+        public bool IsButtonPressed(float mx, float my, Point location, Sprite button) {
+            return mx > location.X && my > location.Y && mx < location.X + button.Width && my < location.Y + button.Height;
         }
     }
 }
