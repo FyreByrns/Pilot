afaca26c018a442fbd4204730c0ed8c213bc0eb1
Gavin White <horace5413@gmail.com>
2020-08-11 20:52:19 -0700

    Allow editing rain.

--- Pilot/Pilot/Editor.cs
+++ Pilot/Pilot/Editor.cs
@@ -9,7 +9,8 @@ namespace Pilot {
     class Editor : Scene {
         enum EditorState {
             Decorations,
-            Actors
+            Actors,
+            Rain
         }
 
         World editing;
@@ -39,40 +40,68 @@ namespace Pilot {
                     PositionableDrawable decoration = GetSelectedDecoration((int)(mx + editing.cameraX), my, lastMX, lastMY);
                     Actor actor = GetSelectedActor((int)(mx + editing.cameraX), lastMX);
 
-                    if (state == EditorState.Decorations) {
-                        if (decoration != null) { // If there's a decoration under the mouse
-                            decoration.x = mx - decoration.GetWidth() / 2 + editing.cameraX;
-                            decoration.y = my - decoration.GetHeight() / 2;
+                    switch (state) {
+                        case EditorState.Decorations:
+                            if (decoration != null) { // If there's a decoration under the mouse
+                                decoration.x = mx - decoration.GetWidth() / 2 + editing.cameraX;
+                                decoration.y = my - decoration.GetHeight() / 2;
 
-                            if (game.GetMouse(PixelEngine.Mouse.Right).Down)
-                                editing.decorations.Remove(decoration);
-                        }
-                        else if (!string.IsNullOrEmpty(currentDecoration))
-                            editing.decorations.Add(new PositionableDrawable(new StaticSprite($"data/decorations/{currentDecoration}.png"), editing.cameraX + mx - 4, my - 4));
-                    }
-                    else
-                    if (state == EditorState.Actors) {
-                        if (my > game.ScreenHeight - 100) { // If the mouse is in NPC area
-                            game.PixelMode = PixelEngine.Pixel.Mode.Alpha;
-                            game.FillRect(new PixelEngine.Point(0, 300 - 100), new PixelEngine.Point(game.ScreenWidth, 300), new PixelEngine.Pixel(255, 255, 255, 10));
-                            game.PixelMode = PixelEngine.Pixel.Mode.Normal;
+                                if (game.GetMouse(PixelEngine.Mouse.Right).Down)
+                                    editing.decorations.Remove(decoration);
+                            }
+                            else if (!string.IsNullOrEmpty(currentDecoration))
+                                editing.decorations.Add(new PositionableDrawable(new StaticSprite($"data/decorations/{currentDecoration}.png"), editing.cameraX + mx - 4, my - 4));
+                            break;
 
-                            if (actor != null) {
-                                actor.x = mx - actor.width / 2 + editing.cameraX;
+                        case EditorState.Actors:
+                            if (my > game.ScreenHeight - 100) { // If the mouse is in NPC area
+                                game.PixelMode = PixelEngine.Pixel.Mode.Alpha;
+                                game.FillRect(new PixelEngine.Point(0, 300 - 100), new PixelEngine.Point(game.ScreenWidth, 300), new PixelEngine.Pixel(255, 255, 255, 10));
+                                game.PixelMode = PixelEngine.Pixel.Mode.Normal;
+
+                                if (actor != null) {
+                                    actor.x = mx - actor.width / 2 + editing.cameraX;
+                                }
+                                else {
+                                    Actor player;
+                                    player = new Actor {
+                                        x = mx - 4 + editing.cameraX,
+                                        y = game.ScreenHeight - 40,
+                                        width = 22,
+                                        height = 40
+                                    };
+                                    player.drawables.Add(new AnimatedSprite("data/animations/possessed", 22));
+                                    ((AnimatedSprite)player.drawables[0]).Play("run_left");
+                                    editing.actors.Add(player);
+                                }
                             }
-                            else {
-                                Actor player;
-                                player = new Actor {
-                                    x = mx - 4 + editing.cameraX,
-                                    y = game.ScreenHeight - 40,
-                                    width = 22,
-                                    height = 40
-                                };
-                                player.drawables.Add(new AnimatedSprite("data/animations/possessed", 22));
-                                ((AnimatedSprite)player.drawables[0]).Play("run_left");
-                                editing.actors.Add(player);
+                            break;
+
+                        case EditorState.Rain:
+                            // Is the mouse near a rain boundary?
+                            World.RainRegion region = null;
+                            bool editingStart = false;
+                            foreach (var r in editing.rainRegions) {
+                                float differenceStart = (mx + editing.cameraX) - r.startX;
+                                float differenceEnd = (mx + editing.cameraX) - r.endX;
+                                float difference = Math.Min(mx - r.startX, mx - r.endX);
+
+                                if (difference < 20) {
+                                    region = r;
+                                    editingStart = differenceStart < differenceEnd;
+                                    break;
+                                }
                             }
-                        }
+
+                            if (region != null) { // If so, edit it.
+                                if (editingStart)
+                                    region.startX = mx + editing.cameraX;
+                                else
+                                    region.endX = mx + editing.cameraX;
+                            }
+                            else  // Otherwise, make a new one.
+                                editing.rainRegions.Add(new World.RainRegion((float)mx + editing.cameraX, (float)mx + editing.cameraX + 20));
+                            break;
                     }
                 }
 
@@ -81,6 +110,7 @@ namespace Pilot {
                     editing.cameraX -= difference;
                 }
                 lastMX = mx;
+                lastMY = my;
 
                 if (game.GetKey(PixelEngine.Key.Control).Down) { // Check for hotkeys
                     game.DrawRect(PixelEngine.Point.Origin, new PixelEngine.Point(game.ScreenWidth - 1, game.ScreenHeight - 1), PixelEngine.Pixel.Presets.DarkRed);
@@ -110,6 +140,8 @@ namespace Pilot {
                         state = EditorState.Decorations;
                     if (game.GetKey(PixelEngine.Key.A).Pressed)
                         state = EditorState.Actors;
+                    if (game.GetKey(PixelEngine.Key.R).Pressed)
+                        state = EditorState.Rain;
                 }
             }
         }
