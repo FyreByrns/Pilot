d3139502eb49f96bed03332d6d89d85074657f40
Gavin White <horace5413@gmail.com>
2020-08-12 08:56:00 -0700

    Player class

--- Pilot/Pilot/PlayState.cs
+++ Pilot/Pilot/PlayState.cs
@@ -7,33 +7,10 @@ using System.Threading.Tasks;
 namespace Pilot {
     class PlayState : Scene {
         World level;
-        Actor player;
-        float playerSpeed = 50f;
-        int lastDir = 0;
+        Player player;
 
         public override void Update(float elapsed) {
             base.Update(elapsed);
-
-            int direction = 0;
-            if (game.GetKey(PixelEngine.Key.Left).Down) direction--;
-            if (game.GetKey(PixelEngine.Key.Right).Down) direction++;
-            player.x += direction * playerSpeed * elapsed;
-
-            if (direction == 0) {
-                if (lastDir >= 0)
-                    player.PlayAnimation("idle_right");
-                else if (lastDir < 0)
-                    player.PlayAnimation("idle_left");
-            }
-            else {
-                if (direction > 0)
-                    player.PlayAnimation("run_right");
-                else
-                if (direction < 0)
-                    player.PlayAnimation("run_left");
-
-                lastDir = direction;
-            }
 
             player.Update(elapsed);
 
@@ -41,7 +18,6 @@ namespace Pilot {
 
             game.Clear(PixelEngine.Pixel.Presets.Lavender);
             level.Draw(game);
-
 
             game.PixelMode = PixelEngine.Pixel.Mode.Alpha;
             player.Draw(game, level.cameraX, 0);
@@ -51,14 +27,7 @@ namespace Pilot {
         public PlayState(Game game, string startingLevel) : base(game) {
             level = new World(startingLevel);
 
-            player = new Actor {
-                x = 10,
-                y = game.ScreenHeight - 40,
-                width = 22,
-                height = 40
-            };
-            player.drawables.Add(new AnimatedSprite("data/animations/flame", 22));
-            ((AnimatedSprite)player.drawables[0]).Play("walk_right");
+            player = new Player(game, 10);
         }
     }
 }
---
+++ Pilot/Pilot/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilot {
+    class Player : Actor {
+        Game game;
+        public Actor attached;
+        public AnimatedSprite animation;
+
+        float speed = 50f;
+        int lastDir = 0;
+
+        public override void Update(float elapsed) {
+            base.Update(elapsed);
+
+            int direction = 0;
+            if (game.GetKey(PixelEngine.Key.Left).Down) direction--;
+            if (game.GetKey(PixelEngine.Key.Right).Down) direction++;
+            x += direction * speed * elapsed;
+
+            if (direction == 0) {
+                if (lastDir >= 0)
+                    PlayAnimation("idle_right");
+                else if (lastDir < 0)
+                    PlayAnimation("idle_left");
+            }
+            else {
+                if (direction > 0)
+                    PlayAnimation("idle_right");
+                else
+                if (direction < 0)
+                    PlayAnimation("idle_left");
+
+                lastDir = direction;
+            }
+        }
+
+        public void Attach(Actor other) {
+
+        }
+
+        public Player(Game game, int x) {
+            this.game = game;
+            this.x = x;
+            animation = new AnimatedSprite("data/animations/flame", 30);
+            animation.Play("walk_right");
+            drawables.Add(animation);
+
+            y = 200 - animation.GetHeight();
+        }
+    }
+}
